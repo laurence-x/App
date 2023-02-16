0c9cf8ff630a00ad8108c5545a7c11bf8c316315
laurence-x <lpemailx@gmail.com>
2023-02-16 15:02:48 +0200

    ok

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,26 @@
-// import { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 // just for testing
 
 const Test = () => {
-    let chars = [ 'A', 'B', 'A', 'C', 'B' ]
+    const [ Res, setRes ] = useState("")
 
-    let Res = arr.filter((x, index) => {
-        return arr.indexOf(x) !== index
-    })
+    useEffect(() => {
+
+        // setRes('xxx')
+
+        let data = new URLSearchParams()
+        data.append("Key", "Value")
+        // http://127.0.0.1:3001/....
+        fetch("path?a=111&b=222", {
+            method: "POST",
+            body: data,
+            // signal: controller.signal,
+        })
+            // .then((r) => r.json())
+            .then((r) => r.text())
+            .then((r) => setRes(r))
+            .catch((e) => setRes(e))
+    }, [])
 
     return <>{Res}</>
 }
